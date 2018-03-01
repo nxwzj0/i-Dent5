4ba683b08780d8aa388cdc5f8faeaa6b8a81f926
nxwzj0 <nxwzj0@126.com>
2018-03-01 10:36:14 +0800

    [#43] ページング修正：顧客モーダル Add Start newtouch

--- src/app/modal/customerSearch.modal.component.ts
+++ src/app/modal/customerSearch.modal.component.ts
@@ -42,15 +42,37 @@ export class CustomerSearchModalComponent {
 
   // ページング処理
   pageChanged(event: any): void {
+    // ::: 2018.03.01 [#43] ページング修正：顧客モーダル Add Start newtouch
+    if (!(this.itemsPerPage > 0)) {
+      this.itemsPerPage = 10;
+    }
+    // ::: 2018.03.01 [#43] ページング修正：顧客モーダル Add End   newtouch
     this.start = this.itemsPerPage * (this.currentPage - 1);
     let tmpStart: number = +this.start;
     let tmpItemsPerPage: number = +this.itemsPerPage;
     this.end = tmpStart + tmpItemsPerPage;
+    // ::: 2018.03.01 [#43] ページング修正：顧客モーダル Add Start newtouch
+    this.search();
+    // ::: 2018.03.01 [#43] ページング修正：顧客モーダル Add End   newtouch
   }
+
+  // ::: 2018.03.01 [#43] ページング修正：顧客モーダル Add Start newtouch
+  // 表示ページを初期化する
+  initCurrentPage() {
+    this.currentPage = 1;
+  }
+  // ::: 2018.03.01 [#43] ページング修正：顧客モーダル Add End   newtouch
 
   // モーダル表示
   openModal() {
     this.clearCustomerSearch();
+    // ::: 2018.03.01 [#43] ページング修正：顧客モーダル Add Start newtouch
+    this.bigTotalItems = 0; // 総数
+    this.itemsPerPage = 10; // 1ページに表示する件数
+    this.currentPage = 0;
+    this.start = 0;
+    this.end = 10;
+    // ::: 2018.03.01 [#43] ページング修正：顧客モーダル Add End   newtouch
     this.template.show();
     this.search();
   }
@@ -70,6 +92,10 @@ export class CustomerSearchModalComponent {
     ps.set("customerCd", this.searchCustomerCd);
     ps.set("customerNm", this.searchCustomerNm);
     ps.set("address", this.searchAddress);
+    // ::: 2018.03.01 [#43] ページング修正：顧客モーダル Add Start newtouch
+    ps.set("pagingStart", (this.start + 1).toString());
+    ps.set("pagingEnd", this.itemsPerPage.toString());
+    // ::: 2018.03.01 [#43] ページング修正：顧客モーダル Add End   newtouch
 
     // 検索
     this.jsonpService.commonRequestGet('CustomerListDataGet.php', ps)
@@ -81,11 +107,16 @@ export class CustomerSearchModalComponent {
           let list = data[0];
           if (list.result !== '' && list.result == true) {
             // 画面表示パラメータのセット処理
+            // ::: 2018.03.01 [#43] ページング修正：顧客モーダル Add Start newtouch
+            this.bigTotalItems = list.count; // ページング(総数)
+            // ::: 2018.03.01 [#43] ページング修正：顧客モーダル Add End   newtouch
             this.setDspParam(data.slice(1)); // 配列1つ目は、サーバ処理成功フラグなので除外
           }
         }
-        this.currentPage = 1;
-        this.pageChanged(null);
+        // ::: 2018.03.01 [#43] ページング修正：顧客モーダル Del Start newtouch
+        // :::         this.currentPage = 1;
+        // :::         this.pageChanged(null);
+        // ::: 2018.03.01 [#43] ページング修正：顧客モーダル Del End   newtouch
         this.isLoading = false;
       },
       error => {
@@ -102,8 +133,10 @@ export class CustomerSearchModalComponent {
   customerList = [];
   // 画面表示パラメータのセット処理
   setDspParam(data) {
-    // ページングの設定
-    this.bigTotalItems = data.length;
+    // ::: 2018.03.01 [#43] ページング修正：顧客モーダル Del Start newtouch
+    // :::     // ページングの設定
+    // :::     this.bigTotalItems = data.length;
+    // ::: 2018.03.01 [#43] ページング修正：顧客モーダル Del End   newtouch
     // 顧客（取引先）リストをセット
     this.customerList = data;
   }
